81d8213a0130a8c37b862d2e14e5fe1b2eb2e5a4
KiriltsevDaniil <Rekvizitpostmodernbit@gmail.com>
2021-07-08 21:31:48 +0300

    Preparation scripts

---
+++ dataset_preparataion/image_cropping.py
@@ -0,0 +1,33 @@
+#image_cropping.py
+import pandas
+from PIL import Image
+i = 0
+k = 1
+signs = []
+forbidden = ['(1)img-1671__2021-06-13-16-21-33-243335.png',
+             '(1)img-1686__2021-06-13-16-21-52-380056.png',
+             '(1)img-1681__2021-06-13-16-21-42-703017.png']
+data = pandas.read_json("./source/markup.json", "index") #why?
+
+while i < len(data):
+    for key in data["regions"][i].keys():
+        signs.append(key)
+        
+    for sign in signs:
+        if "shape_attributes" in data["regions"][i][sign]:
+            x_cords = data["regions"][i][sign]["shape_attributes"]["all_points_x"]
+            y_cords = data["regions"][i][sign]["shape_attributes"]["all_points_y"]
+        else:
+            x_cords = data["regions"][i][sign]["shape-attributes"]["all_points_x"] #why?
+            y_cords = data["regions"][i][sign]["shape-attributes"]["all_points_y"]
+        
+        polygon = [min(x_cords), min(y_cords), max(x_cords), max(y_cords)]
+        image = Image.open("./source/" + data["filename"][i], "r")
+        cropped_image = image.crop(polygon)
+        if f'({k}){data["filename"][i]}' not in forbidden:
+            cropped_image.save(f'./new_dataset/({k}){data["filename"][i]}')
+        k+=1
+    k = 1
+    signs = []
+    i+=1
+print("Dataset cropping complete.")
---
+++ dataset_preparataion/new_markup.py
@@ -0,0 +1,30 @@
+#new_markup.py
+import pandas
+from PIL import Image
+i = 0
+k = 1
+signs = []
+legend = ["name", "width", "height", "class"] #2,3 new set; 1, 4 old set;
+new_data = []
+forbidden = ['(1)img-1671__2021-06-13-16-21-33-243335.png',
+             '(1)img-1686__2021-06-13-16-21-52-380056.png',
+             '(1)img-1681__2021-06-13-16-21-42-703017.png']
+
+data = pandas.read_json("./source/markup.json", "index") #why?
+
+while i < len(data):
+    for key in data["regions"][i].keys(): #collecting signs
+        signs.append(key)
+        
+    for sign in signs:
+        if f'({k}){data["filename"][i]}' not in forbidden:
+            image = Image.open(f'./new_dataset/({k}){data["filename"][i]}', "r") #openning new image
+            w, h = image.size
+            new_data.append([f'({k}){data["filename"][i]}', w, h, sign])
+        k+=1
+    k = 1
+    signs = []
+    i+=1
+df = pandas.DataFrame(new_data, columns=legend)
+df.to_csv("./new_dataset/markup.csv")
+print("Dataset markup complete.")
