b44607387cda9f8760aa88ad555b4feff366f7be
KiriltsevDaniil <71724561+KiriltsevDaniil@users.noreply.github.com>
2021-07-09 00:59:39 +0300

    Update new_markup.py

--- dataset_preparataion/new_markup.py
+++ dataset_preparataion/new_markup.py
@@ -4,21 +4,21 @@ from PIL import Image
 i = 0
 k = 1
 signs = []
-legend = ["name", "width", "height", "class"] #2,3 new set; 1, 4 old set;
+legend = ["name", "width", "height", "class"]
 new_data = []
 forbidden = ['(1)img-1671__2021-06-13-16-21-33-243335.png',
              '(1)img-1686__2021-06-13-16-21-52-380056.png',
              '(1)img-1681__2021-06-13-16-21-42-703017.png']
 
-data = pandas.read_json("./source/markup.json", "index") #why?
+data = pandas.read_json("./source/markup.json", "index")
 
 while i < len(data):
-    for key in data["regions"][i].keys(): #collecting signs
+    for key in data["regions"][i].keys():
         signs.append(key)
         
     for sign in signs:
         if f'({k}){data["filename"][i]}' not in forbidden:
-            image = Image.open(f'./new_dataset/({k}){data["filename"][i]}', "r") #openning new image
+            image = Image.open(f'./new_dataset/({k}){data["filename"][i]}', "r")
             w, h = image.size
             new_data.append([f'({k}){data["filename"][i]}', w, h, sign])
         k+=1
