e5535ac04163378494c7f1e5ac39aab819dce95f
KiriltsevDaniil <71724561+KiriltsevDaniil@users.noreply.github.com>
2021-07-09 21:46:38 +0300

    Update image_cropping.py

--- dataset_preparataion/image_cropping.py
+++ dataset_preparataion/image_cropping.py
@@ -7,7 +7,7 @@ signs = []
 forbidden = ['(1)img-1671__2021-06-13-16-21-33-243335.png',
              '(1)img-1686__2021-06-13-16-21-52-380056.png',
              '(1)img-1681__2021-06-13-16-21-42-703017.png']
-data = pandas.read_json("./source/markup.json", "index")
+data = pandas.read_json("./markup.json", "index")
 
 while i < len(data):
     for key in data["regions"][i].keys():
@@ -22,7 +22,7 @@ while i < len(data):
             y_cords = data["regions"][i][sign]["shape-attributes"]["all_points_y"]
         
         polygon = [min(x_cords), min(y_cords), max(x_cords), max(y_cords)]
-        image = Image.open("./source/" + data["filename"][i], "r")
+        image = Image.open("./" + data["filename"][i], "r")
         cropped_image = image.crop(polygon)
         if f'({k}){data["filename"][i]}' not in forbidden:
             cropped_image.save(f'./new_dataset/({k}){data["filename"][i]}')
