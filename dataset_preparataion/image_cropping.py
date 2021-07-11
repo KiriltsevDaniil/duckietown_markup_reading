a0e17794ce2815e2c30df2107b4f834104aa72d6
KiriltsevDaniil <Rekvizitpostmodernbit@gmail.com>
2021-07-11 13:19:28 +0300

    Mid update

--- dataset_preparataion/image_cropping.py
+++ dataset_preparataion/image_cropping.py
@@ -7,7 +7,7 @@ signs = []
 forbidden = ['(1)img-1671__2021-06-13-16-21-33-243335.png',
              '(1)img-1686__2021-06-13-16-21-52-380056.png',
              '(1)img-1681__2021-06-13-16-21-42-703017.png']
-data = pandas.read_json("./markup.json", "index")
+data = pandas.read_json("../markup.json", "index")
 
 while i < len(data):
     for key in data["regions"][i].keys():
@@ -22,10 +22,10 @@ while i < len(data):
             y_cords = data["regions"][i][sign]["shape-attributes"]["all_points_y"]
         
         polygon = [min(x_cords), min(y_cords), max(x_cords), max(y_cords)]
-        image = Image.open("./" + data["filename"][i], "r")
+        image = Image.open("../dataset/" + data["filename"][i], "r")
         cropped_image = image.crop(polygon)
         if f'({k}){data["filename"][i]}' not in forbidden:
-            cropped_image.save(f'./new_dataset/({k}){data["filename"][i]}')
+            cropped_image.save(f'../new_dataset/({k}){data["filename"][i]}')
         k+=1
     k = 1
     signs = []
--- dataset_preparataion/new_markup.py
+++ dataset_preparataion/new_markup.py
@@ -10,7 +10,7 @@ forbidden = ['(1)img-1671__2021-06-13-16-21-33-243335.png',
              '(1)img-1686__2021-06-13-16-21-52-380056.png',
              '(1)img-1681__2021-06-13-16-21-42-703017.png']
 
-data = pandas.read_json("./markup.json", "index")
+data = pandas.read_json("../markup.json", "index")
 
 while i < len(data):
     for key in data["regions"][i].keys():
@@ -18,7 +18,7 @@ while i < len(data):
         
     for sign in signs:
         if f'({k}){data["filename"][i]}' not in forbidden:
-            image = Image.open(f'./new_dataset/({k}){data["filename"][i]}', "r")
+            image = Image.open(f'../new_dataset/({k}){data["filename"][i]}', "r")
             w, h = image.size
             new_data.append([f'({k}){data["filename"][i]}', w, h, sign])
         k+=1
@@ -26,5 +26,5 @@ while i < len(data):
     signs = []
     i+=1
 df = pandas.DataFrame(new_data, columns=legend)
-df.to_csv("./new_dataset/markup.csv")
+df.to_csv("../markup.csv")
 print("Dataset markup complete.")
